3ce44c417d5e7d3f1593d7989b66134fc97ffb18
Karan Shukla <shuklak13@users.noreply.github.com>
2017-02-05 17:38:25 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int[] input;
+
+            do {
+                Console.WriteLine("In space-separated format, please provide the following arguments:");
+                Console.WriteLine("\tLower Bound (inclusive)");
+                Console.WriteLine("\tUpper Bound (inclusive)");
+                Console.WriteLine("\tQuantity of Numbers (optional)");
+                Console.WriteLine();
+                input = Array.ConvertAll(Console.ReadLine().ToString().Split(' '), int.Parse);
+
+                if (input.Length != 2 && input.Length != 3)
+                    Console.WriteLine("Please give 2 or 3 inputs.");
+            } while (input.Length != 2 && input.Length != 3);
+
+            int min = input[0];
+            int max = input[1];
+            int numIterations = (input.Length == 3) ? input[2] : 1;
+
+            Console.WriteLine();
+            Console.WriteLine("Random Number(s):");
+            for (int i=0; i<numIterations; i++)
+                Console.WriteLine(rnd.Next(min, max+1));
+
+            Console.ReadKey();  //this stops the window from instantly closing
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Random Number Generator
+
+This is a random number generator I wrote in C#
+
+## Motivation
+
+I wanted to generate some random numbers but didn't have an internet connection, so I opened up Visual Studio instead.
